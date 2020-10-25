54be7d833273e4464351691abcd94f5f029505b3
Alice Barbe <alice_barbe@hotmail.com>
2020-10-25 03:47:29 +0000

    Add fruit classification scikit-learn model
    Almost entirely copied from this: https://www.kaggle.com/waltermaffy/fruit-classification-pca-svm-knn-decision-tree/
    
    Requires fruit-360 database to run

---
+++ fruit_classification.py
@@ -0,0 +1,331 @@
+import numpy as np 
+import cv2
+import glob
+import os
+import matplotlib.pyplot as plt
+import string
+from mlxtend.plotting import plot_decision_regions
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.utils.multiclass import unique_labels
+from sklearn import metrics
+from sklearn.svm import SVC
+
+dim = 100
+
+def getYourFruits(fruits, data_type, print_n=False, k_fold=False):
+    images = []
+    labels = []
+    val = ['Training', 'Test']
+    if not k_fold:
+        path = "./fruits-360/" + data_type + "/"
+        for i,f in enumerate(fruits):
+            p = path + f
+            j=0
+            for image_path in glob.glob(os.path.join(p, "*.jpg")):
+                image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+                image = cv2.resize(image, (dim, dim))
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                images.append(image)
+                labels.append(i)
+                j+=1
+            if(print_n):
+                print("There are " , j , " " , data_type.upper(), " images of " , fruits[i].upper())
+        images = np.array(images)
+        labels = np.array(labels)
+        return images, labels
+    else:
+        for v in val:
+            path = "./fruits-360/" + v + "/"
+            for i,f in enumerate(fruits):
+                p = path + f
+                j=0
+                for image_path in glob.glob(os.path.join(p, "*.jpg")):
+                    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+                    image = cv2.resize(image, (dim, dim))
+                    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                    images.append(image)
+                    labels.append(i)
+                    j+=1
+        images = np.array(images)
+        labels = np.array(labels)
+        return images, labels
+    
+def getAllFruits():
+    fruits = []
+    for fruit_path in glob.glob("./fruits-360/Training/*"):
+        fruit = fruit_path.split("/")[-1]
+        fruits.append(fruit)
+    return fruits
+
+# %% Visualize Data
+
+def plot_image_grid(images, nb_rows, nb_cols, figsize=(15, 15)):
+    assert len(images) == nb_rows*nb_cols, "Number of images should be the same as (nb_rows*nb_cols)"
+    fig, axs = plt.subplots(nb_rows, nb_cols, figsize=figsize)
+    
+    n = 0
+    for i in range(0, nb_rows):
+        for j in range(0, nb_cols):
+            axs[i, j].axis('off')
+            axs[i, j].imshow(images[n])
+            n += 1        
+
+def getClassNumber(y):
+    v =[]
+    i=0
+    count = 0
+    for index in y:
+        if(index == i):
+            count +=1
+        else:
+            v.append(count)
+            count = 1
+            i +=1
+    v.append(count)        
+    return v
+
+def plotPrincipalComponents(X, dim):
+    v = getClassNumber(y_train)
+    colors = 'b', 'g', 'r', 'c', 'm', 'y', 'k', 'grey', 'orange', 'purple'
+    markers = ['o', 'x' , 'v', 'd']
+    tot = len(X)
+    start = 0 
+    if(dim == 2):
+        for i,index in enumerate(v):
+            end = start + index
+            plt.scatter(X[start:end,0],X[start:end,1] , color=colors[i%len(colors)], marker=markers[i%len(markers)], label = fruits[i])
+            start = end
+        plt.xlabel('PC1')
+        plt.ylabel('PC2')
+    
+    if(dim == 3):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        for i,index in enumerate(v):
+            end = start + index
+            ax.scatter(X[start:end,0], X[start:end,1], X[start:end,2], color=colors[i%len(colors)], marker=markers[i%len(markers)], label = fruits[i])
+            start = end
+        ax.set_xlabel('PC1')
+        ax.set_ylabel('PC2')
+        ax.set_zlabel('PC3')
+
+
+    plt.legend(loc='lower left')
+    plt.xticks()
+    plt.yticks()
+    plt.show()
+
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = metrics.confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = unique_labels(y_true, y_pred)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=fruits, yticklabels=fruits,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return cm,ax
+
+
+
+# %% Principal Component Analysis
+
+def showPCA(image,X2, X10, X50):
+    fig = plt.figure(figsize=(15,15))
+    ax1 = fig.add_subplot(1,4,1)
+    ax1.axis('off')
+    ax1.set_title('Original image')
+    plt.imshow(image)
+    ax1 = fig.add_subplot(1,4,2)
+    ax1.axis('off') 
+    ax1.set_title('50 PC')
+    plt.imshow(X50)
+    ax1 = fig.add_subplot(1,4,3)
+    ax1.axis('off') 
+    ax1.set_title('10 PC')
+    plt.imshow(X10)
+    ax2 = fig.add_subplot(1,4,4)
+    ax2.axis('off') 
+    ax2.set_title('2 PC')
+    plt.imshow(X2)
+    plt.show()
+
+def computePCA(n, im_scaled, image_id):
+    pca = PCA(n)
+    principalComponents = pca.fit_transform(im_scaled)
+    im_reduced = pca.inverse_transform(principalComponents)
+    newImage = scaler.inverse_transform(im_reduced[image_id])
+    return newImage
+
+def showVariance(X_train):
+    #Compute manually the principal components
+    cov_matr=np.dot(X_train, X_train.T)
+    eigval,eigvect=np.linalg.eig(cov_matr)
+
+    index=np.argsort(eigval)[::-1] #take in order the index of ordered vector (ascending order)
+
+    #eigvect[:,i] is associated to eigval[i] so 
+    eigvect=eigvect[:,index]
+    eigval=eigval[index]
+
+    n_PC=[]
+    var_explained=[]
+    var_temp=[]
+    var_tmp=0
+    for i in range(10):
+        var_tmp=var_tmp+eigval[i]
+        n_PC.append(i)
+        var_temp.append(eigval[i]/(eigval.sum())*100)
+        var_explained.append(var_tmp/(eigval.sum())*100)
+
+    fig, ax = plt.subplots(figsize=(8,8))
+
+    ind = np.arange(10)    
+    width = 0.35         # the width of the bars
+    p1 = ax.bar(ind, var_temp, width, color='b')
+    p2 = ax.bar(ind + width, var_explained, width, color='r')
+
+    ax.legend((p1[0], p2[0]), ('Individual explained variance', 'Cumulative explained variance'))
+
+    ax.set_title('Variance explained using PCs')
+    ax.set_xticks(ind + width / 2)
+    ax.set_xticklabels(('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
+
+    plt.xlabel('Number of PC')
+    plt.ylabel('Variance exaplained in %')
+
+    ax.autoscale_view()
+
+    plt.show()
+    
+
+
+if __name__ == "__main__": 
+    
+    #Choose your Fruits
+    fruits = ['Apple Golden 3' , 'Apple Granny Smith'] #Binary classification
+    
+    #Get Images and Labels 
+    X_t, y_train =  getYourFruits(fruits, 'Training', print_n=True, k_fold=False)
+    X_test, y_test = getYourFruits(fruits, 'Test', print_n=True, k_fold=False)
+    
+    #Get data for k-fold
+    #X,y = getYourFruits(fruits, '', print_n=True, k_fold=True)
+    
+    #Scale Data Images
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform([i.flatten() for i in X_t])
+    X_test = scaler.fit_transform([i.flatten() for i in X_test])
+    #X = scaler.fit_transform([i.flatten() for i in X])
+    
+    print(fruits[y_train[0]])
+    plot_image_grid(X_t[0:100], 10, 10)
+    
+    print(fruits[y_train[490]])
+    plot_image_grid(X_t[490:590], 10, 10)
+    
+    # Data in 2D
+    pca = PCA(n_components=2)
+    dataIn2D = pca.fit_transform(X_train)
+    plotPrincipalComponents(dataIn2D, 2)
+    
+    # Data in 3D
+    pca = PCA(n_components=3)
+    dataIn3D = pca.fit_transform(X_train)
+    plotPrincipalComponents(dataIn3D, 3)
+    
+    # PCA Example
+    
+    image_id = 2
+    image = X_t[image_id]
+    
+    #Compute PCA
+    X_2 = computePCA(2, X_train,image_id)
+    X_10 = computePCA(10, X_train,image_id)
+    X_50 = computePCA(50, X_train,image_id)
+    
+    #Reshape in order to plot images
+    X2 = np.reshape(X_2, (dim,dim,3)).astype(int)
+    X10 = np.reshape(X_10, (dim,dim,3)).astype(int)
+    X50 = np.reshape(X_50, (dim,dim,3)).astype(int)
+    
+    #Plot
+    showPCA(image, X2, X10, X50)
+
+    # Plot variance
+    showVariance(X_train)
+    
+    # Linear SVM
+    
+    svm = SVC(gamma='auto', kernel='linear', probability=True)
+    svm.fit(X_train, y_train) 
+    y_pred = svm.predict(X_test)
+    
+    # Evaluation
+    precision = metrics.accuracy_score(y_pred, y_test) * 100
+    print("Accuracy with SVM: {0:.2f}%".format(precision))
+    cm , _ = plot_confusion_matrix(y_test, y_pred,classes=y_train, normalize=True, title='Normalized confusion matrix')
+    plt.show()
+    '''
+    # calculate the FPR and TPR for all thresholds of the classification
+    probs = svm.predict_proba(X_test)
+    probs = probs[:, 1]
+    svm_fpr, svm_tpr, thresholds = metrics.roc_curve(y_test, probs)
+    svm_auc = metrics.roc_auc_score(y_test, probs)
+    
+    pred_kfold = cross_val_score(svm, X, y, cv=5) 
+    print("Accuracy with SVM and K-FOLD CROSS VALIDATION: %0.2f (+/- %0.2f)" % (pred_kfold.mean(), pred_kfold.std() * 2))
+    
+    # Linear SVM with PCA
+    
+    pca = PCA(n_components=2)
+    X_train2D = pca.fit_transform(X_train)
+    X_test2D = pca.fit_transform(X_test)
+    
+    svm.fit(X_train2D, y_train) 
+    test_predictions = svm.predict(X_test2D)
+    precision = metrics.accuracy_score(test_predictions, y_test) * 100
+    print("Accuracy with SVM considering only first 2PC: {0:.2f}%".format(precision))
+    '''
